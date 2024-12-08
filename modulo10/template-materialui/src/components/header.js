535ab45bfc5647d632c6ce4f8dcfbf0e5ff9aea6
lucasfreitas00dat <l.exe.dat@gmail.com>
2024-12-07 23:42:14 -0300

    Primeiro push do Projeto Faculdade

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -5,31 +5,41 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
- 
+import MenuIcon from '@mui/icons-material/Menu';  // Icone de menu (opcional)
+
 function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky" sx={{ backgroundColor: '#282c34' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* Ícone de menu (descomentado caso queira adicionar funcionalidade de menu) */}
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          
+          {/* Título do cabeçalho */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Gerenciamento de Projetos
+          </Typography>
+
+          {/* Botão de ação no cabeçalho */}
+          <Button
+            color="inherit"
+            sx={{ fontSize: '16px', fontWeight: 'bold' }}
+            onClick={() => alert("Botão clicado!")}
+          >
+            Adicionar Tarefa
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
- 
+
 export default Header;
